d087ba28f5fe034301e69ee6b46ac6c169c040ce
VorDeau <j5musyaf@gmail.com>
2026-03-11 19:11:17 +0700

    feat: update Content Security Policy for enhanced security and modify preconnect links

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,10 @@ export default defineNuxtConfig({
             "Wait for a YouTube channel to go live. Auto-redirects when stream is detected.",
         },
         { name: "theme-color", content: "#080808" },
+        { 
+          "http-equiv": "Content-Security-Policy", 
+          content: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.youtube.com https://s.ytimg.com https://static.cloudflareinsights.com; frame-src 'self' https://www.youtube.com https://www.youtube-nocookie.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://i.ytimg.com; connect-src 'self' https://www.youtube.com https://www.youtube-nocookie.com;" 
+        }
       ],
       link: [
         { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
@@ -33,7 +37,7 @@ export default defineNuxtConfig({
           rel: "stylesheet",
           href: "https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Geist+Mono:wght@300;400;500&display=swap",
         },
-        { rel: "preconnect", href: "https://www.youtube.com" },
+        { rel: "preconnect", href: "https://www.youtube-nocookie.com" },
         { rel: "preconnect", href: "https://s.ytimg.com" },
         { rel: "dns-prefetch", href: "https://www.youtube.com" },
         { rel: "dns-prefetch", href: "https://www.google.com" },
